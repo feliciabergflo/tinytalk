83c7af14b00ea7b54932964a2d52694e2588a8ac
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-04-11 18:22:01 +0200

    Called Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/css/Common.css";
+import Register from "./components/loggedOut/Register";
 
 function App() {
   return (
     <div className="App">
       <h1>TinyTalk</h1>
+      <Register />
     </div>
   );
 }
